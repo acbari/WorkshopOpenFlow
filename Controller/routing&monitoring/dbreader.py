8d7ea743a35fce58e24a449b35da6241bb7753f3
akbari <acbarie@s.itb.ac.id>
2019-08-07 14:30:34 +0700

    Update dbreader.py

--- Controller/routing&monitoring/dbreader.py
+++ Controller/routing&monitoring/dbreader.py
@@ -2,6 +2,6 @@ import redis,sys
 name = sys.argv[1]
 
 rd = redis.Redis(host='localhost', port=6379, db=0)
-print "Time","Pkt rcv","Pkt trx","Byte rcv","Byte trx"
+print "Time","Pkt_rcv","Pkt_trx","Byte_rcv","Byte_trx"
 for i in range(rd.llen(name)):
 	print rd.lindex(name,i)
