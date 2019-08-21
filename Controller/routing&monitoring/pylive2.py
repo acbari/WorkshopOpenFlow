0f0b615fa45fad237141b2711db365088116cdaa
akbari <acbarie@s.itb.ac.id>
2019-08-21 14:28:03 +0700

    Update pylive2.py

--- Controller/routing&monitoring/pylive2.py
+++ Controller/routing&monitoring/pylive2.py
@@ -1,40 +1,63 @@
-#from pylive import live_plotter
-from pylive2 import live_plotter
+import matplotlib.pyplot as plt
 import numpy as np
-import time, sys
-import redis
 
-rd = redis.Redis(host='localhost', port=6379, db=0)
-argval = sys.argv[1]
-prmval = int(sys.argv[2])
-size = 100
-x_vec = np.linspace(0,1,size+1)[0:-1]
-y_vec = np.random.randn(len(x_vec))
-line1 = []
-line2 = []
-data = {}
-while True:
-	yval = []
-	yval2 = []
-	xval = []
-	maxlist = rd.llen(argval)
-	if maxlist > size+1:
-		j = 0
-		for i in range(maxlist-(size+1),maxlist):
-			strval = rd.lindex(argval,i)
-			print strval
-			datastr = strval.split(",")
-			data[j] = int( datastr[prmval] )
-			if j > 0:
-				yval.append(data[j])
-				yval2.append(data[j] - data[j-1])
-				xval.append((5*j)-size*5)
-			j+=1
-	#rand_val = np.random.randn(1)
-	#y_vec[-1] = rand_val
-	x_vec = np.array(xval)
-	y_vec = np.array(yval)
-	y_vec2 = np.array(yval2)
-	line1,line2 = live_plotter(x_vec,y_vec,y_vec2,line1,line2)
-	y_vec = np.append(y_vec[1:],0.0)
-	time.sleep(0.5)
+# use ggplot style for more sophisticated visuals
+#plt.style.use('ggplot')
+dt1min = 0
+dt1max = 0
+dt2min = 0
+dt2max = 0
+ax1 = None
+ax2 = None
+isinit = False
+
+def live_plotter(x_vec,y1_data,y2_data,line1,line2,identifier='',pause_time=0.1): 
+	global isinit
+	global ax1
+	global ax2
+	if line1==[]:
+		# this is the call to matplotlib that allows dynamic plotting
+		plt.ion()
+		fig = plt.figure(figsize=(13,6))
+		ax1 = fig.add_subplot(121)
+		ax2 = fig.add_subplot(122)
+		ax1.title.set_text('Aggregated data')
+		ax2.title.set_text('Real time data')
+		
+		# create a variable for the line so we can later update it
+		line1, = ax1.plot(x_vec,y1_data,'-o',alpha=0.8)        
+		line2, = ax2.plot(x_vec,y2_data,'-o',alpha=0.8)    
+		dt1min = np.min(y1_data)
+		dt1max = np.max(y1_data)
+		dt2min = np.min(y2_data)
+		dt2max = np.max(y2_data)
+		line2.set_ydata(y2_data)    
+		#if np.min(y1_data)<=line1.axes.get_ylim()[0] or np.max(y1_data)>=line1.axes.get_ylim()[1]:
+		ax1.set_ylim(dt1min-10,dt1max+10)
+		#if np.min(y2_data)<=line2.axes.get_ylim()[0] or np.max(y2_data)>=line2.axes.get_ylim()[1]:
+		ax2.set_ylim(dt2min-1,dt2max+1)
+		
+		#update plot label/title
+		plt.ylabel('Numbers')
+		#plt.title('Title: {}'.format(identifier))
+        	plt.show()
+		isinit = True
+    	if isinit:
+		dt1min = np.min(y1_data)
+		dt1max = np.max(y1_data)
+		dt2min = np.min(y2_data)
+		dt2max = np.max(y2_data)
+		line2.set_ydata(y2_data) 
+		ax1.set_ylim(dt1min-10,dt1max+10)
+		ax2.set_ylim(dt2min-1,dt2max+1)
+
+	# after the figure, axis, and line are created, we only need to update the y-data
+	line1.set_ydata(y1_data)
+	line2.set_ydata(y2_data)
+	# adjust limits if new data goes beyond bounds
+	
+	# this pauses the data so the figure/axis can catch up - the amount of pause can be altered above
+	plt.pause(pause_time)
+	
+	# return line so we can update it again in the next iteration
+    	return line1,line2
